3842b3e3e072e492654e272974a868973fad1843
Rishita Patel <rrpatel42@myseneca.ca>
2022-01-13 19:39:08 -0500

    Submitting WS00

---
+++ Workshop_zero/code.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	for (i = 0; i < 6; i++)
+	{
+		cout << "Hey there, I'm Rishita Patel...." << endl;
+	}
+
+	cout << endl << "Hope you all are doing well and safe :)" << endl;
+	return 0;
+}
